347fbb18906b1a4ed7385244d0783b6bb580d185
AmandaCornelsenC <amandacornelsen.c@gmail.com>
2025-06-05 23:38:02 -0300

    first commit

---
+++ app/src/main/java/amandacornelsen/github/com/alunos_rm98160_rm97760/MainActivity.kt
@@ -0,0 +1,21 @@
+package amandacornelsen.github.com.alunos_rm98160_rm97760
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import amandacornelsen.github.com.alunos_rm98160_rm97760.ui.theme.Alunos_rm98160_rm97760Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
